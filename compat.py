9c1a05a4e119e43c9cb12e65c11f7f551073763f
stratospher <44024636+stratospher@users.noreply.github.com>
2021-12-16 19:11:24 +0530

    Add ChaCha20, Poly1305 from py-chacha20poly1305

---
+++ compat.py
@@ -0,0 +1,38 @@
+# Source : https://github.com/ph4r05/py-chacha20poly1305
+
+# Author: Trevor Perrin
+# See the LICENSE file for legal information regarding use of this file.
+
+"""Miscellaneous functions to mask Python version differences."""
+
+import sys
+import platform
+import hmac
+
+if sys.version_info >= (3,0):
+    def compat26Str(x): return x
+
+else:
+    # Python 2.6 requires strings instead of bytearrays in a couple places,
+    # so we define this function so it does the conversion if needed.
+    # same thing with very old 2.7 versions
+    # or on Jython
+    if sys.version_info < (2, 7) or sys.version_info < (2, 7, 4) or platform.system() == 'Java':
+        def compat26Str(x): return str(x)
+    else:
+        def compat26Str(x): return x
+
+
+if hasattr(hmac, 'compare_digest'):
+    ct_compare_digest = hmac.compare_digest
+else:
+    def ct_compare_digest(val_a, val_b):
+        """Compares if string like objects are equal. Constant time."""
+        if len(val_a) != len(val_b):
+            return False
+
+        result = 0
+        for x, y in zip(val_a, val_b):
+            result |= x ^ y
+
+        return result == 0
---
+++ cryptomath.py
@@ -0,0 +1,19 @@
+# Source : https://github.com/ph4r05/py-chacha20poly1305
+
+# Authors:
+#   Trevor Perrin
+#   Martin von Loewis - python 3 port
+#   Yngve Pettersen (ported by Paul Sokolovsky) - TLS 1.2
+#
+# See the LICENSE file for legal information regarding use of this file.
+
+"""cryptomath module
+
+This module has basic math/crypto code."""
+from __future__ import print_function
+
+
+def divceil(divident, divisor):
+    """Integer division with rounding up"""
+    quot, r = divmod(divident, divisor)
+    return quot + int(bool(r))
